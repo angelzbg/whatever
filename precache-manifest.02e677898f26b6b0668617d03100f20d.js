66dc2344393edbfffcc3867654ebd915d545dfcb
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2019-12-10 16:16:08 +0200

    Updates

---
+++ precache-manifest.02e677898f26b6b0668617d03100f20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b8577c374e5ba2b93f91fe19b91912",
+    "url": "/whatever/index.html"
+  },
+  {
+    "revision": "ab07fc74d6fd222dab1a",
+    "url": "/whatever/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "16bc43cce3f726b27b61",
+    "url": "/whatever/static/js/2.02bbf34b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/whatever/static/js/2.02bbf34b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab07fc74d6fd222dab1a",
+    "url": "/whatever/static/js/main.ebf36e31.chunk.js"
+  },
+  {
+    "revision": "3add7ae9053c4273adbe",
+    "url": "/whatever/static/js/runtime-main.14e43e02.js"
+  }
+]);
